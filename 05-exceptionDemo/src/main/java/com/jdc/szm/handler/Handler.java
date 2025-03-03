d88b71074f0cef70251faee050e1223821a9b749
JM505 Computers <JM505 Computers@ShenZinMao>
2025-03-03 11:34:40 +0630

    a

--- 05-exceptionDemo/src/main/java/com/jdc/szm/handler/Handler.java
+++ 05-exceptionDemo/src/main/java/com/jdc/szm/handler/Handler.java
@@ -2,4 +2,10 @@ package com.jdc.szm.handler;
 
 public class Handler {
 
+	private Person person;
+	
+	void useMessage() {
+		person.setName("John Doe");
+		System.out.println(person.getName());
+	}
 }
